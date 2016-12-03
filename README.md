0ec4d3735f7151d80beaa885b1983008683f2418
Albert Martin <albert@imor.tl>
2016-12-03 17:54:37 -0600

    feat(feed): add express server with podcast feed

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## Getting started
 
-Run `npm install` and then `npm start` to boot a development server at
+Run `npm install` and then `npm run dev` to boot a development server at
 http://localhost:8000. Changes to assets and partials will be recompiled through
 Webpack and the browser will be refreshed automatically.
---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const http = require('needle');
+const JTS = require('jts');
+
+var app = express();
+var engine = new JTS();
+
+app.set('views', 'src/views');
+app.engine('jts', engine.render);
+app.use(express.static('dist'));
+
+app.get('/:id.xml', (req, res) => {
+  getPodcast(req.params.id).then(payload => {
+    res.header('Content-Type', 'text/xml; charset=UTF-8');
+    res.render('feed.jts', payload);
+  }).catch(err => {
+    res.status(404).send(err);
+  });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log(`Server running on port ${this.address().port}.`)
+});
+
+function getPodcast(id) {
+  let payload = {};
+  return new Promise((resolve, reject) => {
+    Promise.all([
+      api(`podcast/${id}`),
+      api(`podcast/${id}/media?sort=-date`)
+    ]).then(podcast => {
+      payload.podcast = podcast[0];
+      payload.podcastMedia = podcast[1];
+      return api(`ministry/${payload.podcast.ministry}`);
+    }).then(ministry => {
+      payload.ministry = ministry;
+      resolve(payload);
+    }).catch(err => {
+      reject(err);
+    });
+  });
+}
+
+function api(endpoint) {
+  return new Promise((resolve, reject) => {
+    http.get(`http://api.bethel.io/${endpoint}`, (err, response) => {
+      if (err || response.statusCode !== 200) {
+        return reject(err || response);
+      }
+      resolve(response.body);
+    });
+  });
+}
