2afdb0dbded20cd8fbe28b8fa717f928803eacf7
Albert Martin <albert@imor.tl>
2017-06-01 22:20:30 -0500

    fix(api): replace needle with request

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const http = require('needle');
 const JTS = require('jts');
+const request = require('request');
 
 const xmlescape = require('xml-escape');
 
@@ -17,20 +17,24 @@ app.enable('trust proxy');
 
 app.get('/:id.xml', (req, res) => {
   getPodcast(req.params.id).then(payload => {
-    http.post('https://api.bethel.io/performance/track', {
-      collection: 'podcast',
-      podcast: payload.podcast._id,
-      ministry: payload.ministry._id,
-      ip_address: req.ip,
-      user_agent: req.headers['user-agent'],
-    }, { json: true }, (err, response) => {
+    request.post({
+      url: 'https://api.bethel.io/performance/track',
+      body: {
+        collection: 'podcast',
+        podcast: payload.podcast._id,
+        ministry: payload.ministry._id,
+        ip_address: req.ip,
+        user_agent: req.headers['user-agent'],
+      },
+      json: true,
+    }, err => {
       if (err) console.error(err);
     });
 
     res.header('Content-Type', 'text/xml; charset=UTF-8');
     res.render('feed.jts', payload);
   }).catch(err => {
-    res.status(404).send(err);
+    res.status(404).send();
   });
 });
 
@@ -55,7 +59,6 @@ function getPodcast(id) {
       payload.ministry = ministry.data;
       resolve(payload);
     }).catch(err => {
-      console.error(err);
       reject(err);
     });
   });
@@ -63,12 +66,19 @@ function getPodcast(id) {
 
 function api(endpoint) {
   return new Promise((resolve, reject) => {
-    http.get(`https://api.bethel.io/${endpoint}`, { json: true }, (err, response) => {
+    request.get(`https://api.bethel.io/${endpoint}`, (err, response, body) => {
       if (err || response.statusCode !== 200) {
-        console.error(err || response);
-        return reject(err || response);
+        console.error(`API request to /${endpoint} failed:`, err || response.statusCode);
+        return reject(err || response.statusCode);
       }
-      resolve(response.body);
-    });
+
+      try {
+        body = JSON.parse(body);
+        resolve(body);
+      } catch(err) {
+        console.error(`Failed to parse response from /${endpoint}: `, err.message || err);
+        reject(err.message || err);
+      }
+    })
   });
 }
