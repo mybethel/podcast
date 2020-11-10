2efd0f3a0b95cc80dd5be00e96a1c30298a746c9
Albert Martin <albert@imor.tl>
2020-11-09 19:47:21 -0600

    feat(api): add download endpoint

---
+++ api/download/index.spec.js
@@ -0,0 +1,40 @@
+/**
+ * @jest-environment node
+ */
+const fetch = require('cross-fetch')
+const request = require('supertest')
+
+const handler = require('./')
+
+jest.mock('cross-fetch')
+
+describe('/download/:id', () => {
+  it('requires the media ID as a parameter', async () => {
+    expect((await request(handler).get('/download')).status).toBe(404)
+    expect((await request(handler).get('/download/foo')).status).toBe(404)
+    expect(fetch).not.toHaveBeenCalled()
+  })
+
+  it('uses the API to lookup media', async () => {
+    fetch.mockResolvedValue({ json: () => ({ data: null }) })
+    await request(handler).get('/download/55d9ffb57c52600b006e5bdc.mp3')
+    expect(fetch).toHaveBeenCalled()
+
+    const { body } = fetch.mock.calls.pop().pop()
+    expect(JSON.parse(body).variables).toBeDefined()
+    expect(JSON.parse(body).variables.id).toBe('55d9ffb57c52600b006e5bdc')
+  })
+
+  it('returns a 404 when the media is not found', async () => {
+    fetch.mockResolvedValue({ json: () => ({ data: { asset: null } }) })
+    expect((await request(handler).get('/download/55d9ffb57c52600b006e5bdc')).status).toBe(404)
+  })
+
+  it('redirects to the underlying media url', async () => {
+    fetch.mockResolvedValue({ json: () => ({ data: { asset: { url: 'https://player.vimeo.com/external/477368864.sd.mp4' } } }) })
+
+    const { status, headers, ...rest } = await request(handler).get('/download/55d9ffb57c52600b006e5bdc')
+    expect(status).toBe(302)
+    expect(headers.location).toContain('player.vimeo.com')
+  })
+})
