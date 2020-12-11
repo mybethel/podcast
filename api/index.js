284429fc6f4d210caf2d0d7c6bc09eb3c41f3fa1
Albert Martin <albert@imor.tl>
2020-12-10 19:38:23 -0600

    chore: remove tracking call from feed

--- api/index.js
+++ api/index.js
@@ -12,13 +12,6 @@ module.exports = async (req, res) => {
   if (path.ext !== '.xml' || !path.name) return send(res, 404)
 
   const payload = await getPodcast(path.name)
-  client.post('performance/track', {
-    collection: 'podcast',
-    podcast: payload.podcast._id,
-    ministry: payload.ministry._id,
-    ip_address: req.ip,
-    user_agent: req.headers['user-agent']
-  })
 
   res.setHeader('Content-Type', 'text/xml; charset=UTF-8')
   send(res, 200, feed(payload))
