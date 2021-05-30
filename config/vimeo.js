60be915546969121d31aa80d2a85a9df7c699a60
Albert Martin <albert@imor.tl>
2021-05-29 23:58:58 -0500

    wip: fetch all pages and filter based on tags

---
+++ config/vimeo.js
@@ -0,0 +1,4 @@
+module.exports = {
+  key: '4990932cb9c798b238e98108b4890c59497297ba',
+  secret: process.env.VIMEO_SECRET
+}
--- scripts/sync.js
+++ scripts/sync.js
@@ -1,100 +1,76 @@
-'use strict';
+const { resolve } = require('path')
 
-const moment = require('moment');
-const got = require('got');
+const config = require('@nautilus/config')(resolve(__dirname, '../config'))
+const { MongoClient } = require('mongodb')
+const fetch = require('node-fetch')
 
-exports.handler = function(event, context) {
-  const key = event.auth.key;
-  const secret = event.auth.secret;
-  const token = event.token;
+;(async () => {
+  const client = await MongoClient.connect(config.connections.mongo, {
+    useUnifiedTopology: true
+  })
 
-  const tags = event.tags.split(',').map(tag => tag.trim());
+  const cursor = client.db().collection('podcast').aggregate([
+    { $match: { source: 2, deleted: { $ne: true } } },
+    { $lookup: { from: 'service', localField: 'service', foreignField: '_id', as: 'service' } },
+    { $unwind: { path: '$service', preserveNullAndEmptyArrays: true } }
+  ])
 
-  getVideos(token)
-    .then(checkPages(token, tags))
-    .then(context.succeed)
-    .catch(context.fail);
-};
+  // @TODO: Parallelize without blowing out our Rate Limit
+  // @see https://developer.vimeo.com/guidelines/rate-limiting
+  for await (const podcast of cursor) {
+    const log = msg => console.log([podcast._id, podcast.name, msg].join(' - '))
 
-/**
- * Simple wrapper around the Vimeo API to query a user for all their videos.
- * This hits the `/me/videos` endpoint on Vimeo which uses the `token` to
- * determine which user to return videos for.
- * @param {String} token User token for the Vimeo API.
- * @param {Function} cb Callback once the request has been fulfilled.
- * @param {Number} page Optional page number for pagination.
- */
-function getVideos(token, page) {
-  const route = `/me/videos?page=${page || 1}&per_page=50`;
-  return got(`https://api.vimeo.com${route}`, {
-    headers: {
-      'Authorization': `Bearer ${token}`,
-      'User-Agent': 'Bethel Technologies <hello@bethel.io>'
-    },
-    json: true
-  });
-}
-
-/**
- * Process the first page of results and determine how many pages to sync. This
- * allows for the sync process to finish faster. When more pages are present,
- * each additional page is fetched in parallel. Once all pages have been fetched
- * every page of results is parsed.
- * @param {String} token User token for the Vimeo API.
- * @param {Array} tags All tags which should be used to match.
- */
-function checkPages(token, tags) {
-  const tagsToSync = tags.toString().toLowerCase();
-
-  return function firstPageCallback(response) {
-    let additionalPages = 0;
-    if (response.body.total > 50) {
-      additionalPages = Math.ceil((response.body.total - 50) / 50);
+    if (!podcast.service || !podcast.sourceMeta) {
+      log('Vimeo account or tags not defined')
+      continue
     }
 
-    let pageRequests = [];
-    for (var i = 2; i <= additionalPages + 1; i++) {
-      pageRequests.push(getVideos(token, i));
-    }
+    const results = await getVideos(podcast)
+    log(`${results.length} matching videos`)
 
-    return Promise.all(pageRequests).then(results => {
-      results.unshift(response);
-      return processResults(results, tags);
-    });
+    throw new Error('@TODO')
   }
-}
+
+  console.log('done')
+  process.exit(0)
+})()
 
 /**
- * Process all pages of Vimeo results and determine which videos match the
- * criteria to sync. This is solely based on the tags, although by default any
- * videos which have not been marked as public will be ignored.
- * @param {Array} results Each page of results returned from the Vimeo API.
- * @param {Array} tags All tags which should be used to match.
+ * Fetch all videos for the Podcast that match the defined tags.
+ * @param {Object} podcast
+ * @param {Number} page
+ * @param {Array} result
+ * @returns {Promise<Array>}
  */
-function processResults(results, tags) {
-  const tagsToSync = tags.toString().toLowerCase();
-  let matchingVideos = [];
+const getVideos = async (podcast, page = 1, result = []) => {
+  const { service: { accessToken }, sourceMeta } = podcast
+  const tagsToSync = sourceMeta.toString().toLowerCase()
 
-  return new Promise(resolve => {
-    results.forEach(response => {
-      matchingVideos = matchingVideos.concat(response.body.data.filter(video => {
-        // If a video is not marked as public it is ignored. In the future this
-        // could be modified to allow private channels to be used as a source.
-        if (video.privacy && video.privacy.view !== 'anybody') {
-          return false;
-        }
+  console.log(`page ${page}`)
+  const { paging, data } = await (await fetch(`https://api.vimeo.com/me/videos?page=${page}&per_page=100`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  })).json()
 
-        // Simple string matching is used to determine if any of the tags from
-        // Vimeo match the tags passed to this function.
-        let matches = video.tags.map(tag =>
-                        tagsToSync.indexOf(tag.name.toLowerCase()) >= 0
-                      ).filter(match => match === true);
+  result.push(...data.filter(video => {
+    // If a video is not marked as public it is ignored. In the future this
+    // could be modified to allow private channels to be used as a source.
+    if (video.privacy && video.privacy.view !== 'anybody') {
+      return false
+    }
 
-        // If at least one tag matches, the video should be included.
-        return matches.length > 0;
-      }));
-    });
+    // Simple string matching is used to determine if any of the tags from
+    // Vimeo match the tags passed to this function.
+    const matches = video.tags.map(tag =>
+      tagsToSync.indexOf(tag.name.toLowerCase()) >= 0
+    ).filter(match => match === true)
 
-    resolve(matchingVideos);
-  });
+    // If at least one tag matches, the video should be included.
+    return matches.length > 0
+  }))
+
+  return paging.next
+    ? getVideos(podcast, page + 1, result)
+    : result
 }
